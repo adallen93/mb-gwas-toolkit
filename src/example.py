0f0cf66bd2f13c9b65b7f38b2a0a1d0583223545
Austin Allen <92394410+adallen93@users.noreply.github.com>
2024-04-11 18:43:06 -0400

    Initial commit

---
+++ src/example.py
@@ -0,0 +1,21 @@
+"""Test example.
+
+Other things.
+"""
+
+
+def main() -> None:
+    """Run the example."""
+    pass
+
+
+def do_something() -> int:
+    """Do something else."""
+    a = 5
+    b = a * 3
+    c = a + b + a + b + a + b + a + b + a + b + a + b + a + b + a + b + a + b
+    return c
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_example.py
@@ -0,0 +1,12 @@
+"""Example tests."""
+from example import do_something, main
+
+
+def test_example() -> None:
+    """Test example."""
+    main()
+
+
+def test_something() -> None:
+    """Test something else."""
+    do_something()
