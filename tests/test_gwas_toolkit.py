677b0cd386aacf299ecbc33e4cfb84fc80284ae9
Wally <jh865@duke.edu>
2024-04-27 22:55:54 -0400

    Jonathan's 11th Commit (with comments for Julia's tests)

--- tests/test_gwas_toolkit.py
+++ tests/test_gwas_toolkit.py
@@ -1,1 +1,19 @@
 """Tests For The GWAS Toolkit."""
+
+# conn = sqlite3.connect(":memory:")
+# Parse_GWAS_Output("tests/data_for_testing.csv", ",", conn)
+# gwas_obj = GWAS_Object(conn)
+# print(gwas_obj.N_of_Significant_Tests)
+# print(gwas_obj.Alpha_Level)
+# print(gwas_obj.Manhattan_Plot)
+# print(gwas_obj.QQ_Plot)
+
+# Jonathan's Comments for Julia
+# Show that the parse data function works well.
+# Build tests to show all the GWAS_Object functions work.
+#     The tricky thing: build tests proving that plot functions print plots.
+#     This is pretty hard, but I think you can do it!
+# Build tests that raises all error conditions.
+# For some of these tests, you need to call data_for_testing.csv.
+# But most of these tests don't need data_for_testing.csv.
+# Instead, you can make your own toy data to prove, eg., that an error occurs.
