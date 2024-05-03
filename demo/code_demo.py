2fca32b958bd55bc7d3f5bd3c36afc4a889b4ca2
Wally <jh865@duke.edu>
2024-05-03 01:53:25 -0400

    Finishing Touches

--- demo/code_demo.py
+++ demo/code_demo.py
@@ -1,9 +1,6 @@
 """Demo For The GWAS Toolkit."""
 # Run The Following Code To See What gwas_toolkit Does.
 
-# Remember To First Run The Following Command In A Bash Terminal:
-# pip install git+https://github.com/adallen93/mb-gwas-toolkit@main
-
 import sqlite3
 
 from gwas_toolkit import GWAS_Object, Parse_GWAS_Output
